eaa8d7932ff230546020131a9efeab371043e1ac
rayssa.andrade <rayssa.andrade071@outlook.com>
2021-11-13 15:14:40 -0300

    criando migrations e banco de dados

---
+++ geneticAlgorithmsApp/Migrations/20211113181032_InitialCreate.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace geneticAlgorithmsApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Curso",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Curso", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Disciplina",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Creditos = table.Column<int>(type: "int", nullable: false),
+                    Periodo = table.Column<int>(type: "int", nullable: false),
+                    DisciplinaId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Disciplina", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Disciplina_Disciplina_DisciplinaId",
+                        column: x => x.DisciplinaId,
+                        principalTable: "Disciplina",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Local",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Local", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Professor",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Professor", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Turma",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    DiaDaSemana = table.Column<int>(type: "int", nullable: false),
+                    HorarioInicio = table.Column<TimeSpan>(type: "time", nullable: false),
+                    HorarioFim = table.Column<TimeSpan>(type: "time", nullable: false),
+                    LocalId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    CursoId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    ProfessorId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    DisciplinaId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Turma", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Turma_Curso_CursoId",
+                        column: x => x.CursoId,
+                        principalTable: "Curso",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Turma_Disciplina_DisciplinaId",
+                        column: x => x.DisciplinaId,
+                        principalTable: "Disciplina",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Turma_Local_LocalId",
+                        column: x => x.LocalId,
+                        principalTable: "Local",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Turma_Professor_ProfessorId",
+                        column: x => x.ProfessorId,
+                        principalTable: "Professor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Disciplina_DisciplinaId",
+                table: "Disciplina",
+                column: "DisciplinaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Turma_CursoId",
+                table: "Turma",
+                column: "CursoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Turma_DisciplinaId",
+                table: "Turma",
+                column: "DisciplinaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Turma_LocalId",
+                table: "Turma",
+                column: "LocalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Turma_ProfessorId",
+                table: "Turma",
+                column: "ProfessorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Turma");
+
+            migrationBuilder.DropTable(
+                name: "Curso");
+
+            migrationBuilder.DropTable(
+                name: "Disciplina");
+
+            migrationBuilder.DropTable(
+                name: "Local");
+
+            migrationBuilder.DropTable(
+                name: "Professor");
+        }
+    }
+}
---
+++ geneticAlgorithmsApp/src/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using geneticAlgorithmsApp.src.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace geneticAlgorithmsApp.src.Data
+{
+    class DataContext : DbContext
+    {
+        public DbSet<Curso> Curso { get; set; }
+        public DbSet<Disciplina> Disciplina { get; set; }
+        public DbSet<Local> Local { get; set; }
+        public DbSet<Professor> Professor { get; set; }
+        public DbSet<Turma> Turma { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    "Integrated Security=SSPI; Password=123; User ID=sa; Initial Catalog=GeneticAlgorithms; Data Source=LAPTOP-02VG7CTT\\SQLEXPRESS"
+                    );
+            }
+        }
+    }
+}
